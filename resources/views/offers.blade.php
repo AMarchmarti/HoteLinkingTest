2f0ede5bb3842dab53c64943bb52cdb7621b1861
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 00:16:13 +0100

    creacion de las ofertas

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -2,5 +2,13 @@
 
 
 @section('content')
-    
+    <p>Aquí encontrará diferentes ofertas que le puedan interesar, ya sean noches gratuitas, descuentos para hoteles, entre otros...</p>
+    @if(count ($offers) > 0)
+        @foreach($offers as $offer)
+            <div class="wll">
+                <h3>{{ $offer->Offers_name }}</h3>
+                <p>{{ $offer->description}}</p>
+            </div>
+        @endforeach
+    @endif
 @endsection
