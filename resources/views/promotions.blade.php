9e04a35626a18b7f322615eea505f10c5359c65c
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 11:44:59 +0100

    creacion del cuerpo de la pagina

--- resources/views/promotions.blade.php
+++ resources/views/promotions.blade.php
@@ -2,5 +2,31 @@
 
 
 @section('content')
+<div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel">
+                    <div class="panel-header">Promotions</div>
     
+                    <div class="panel-body">
+                        <h4>Aquí tienes los cupones canjeados</h4>
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Ofertas seleccionadas</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($promotions as $promotion)
+                                <tr>
+                                    <th>{{ $offerts->title }}</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                
+                            @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
