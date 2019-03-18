ea43a299efe77e80a235f623eb890c7c786dc51f
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 12:25:46 +0100

    mensaje de entrada

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Laravel!</h1>
-        <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p>
+        <h1>Bienvenido!</h1>
+        <p>Aquí encontrarás un montón de ofertas interesantes para poder viajar dónde tú quieras, no esperes más.<br> 
+            Podrás viajar desde las tierras de los gigantes de hielo hasta las maravillosas playas del caribe, sólo tienes que registrarte y canjear el cupón. </p>
         <p><a class="btn btn-primary btn-lg" href="http://localhost/HoteLinkingTest/public/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="http://localhost/HoteLinkingTest/public/register" role="button">Register</a></p>
     </div>
 @endsection
