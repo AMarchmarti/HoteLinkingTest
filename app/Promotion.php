e8e187d6526ff46463de0a63dfe77e0d10095bab
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 11:43:26 +0100

    atributos

--- app/Promotion.php
+++ app/Promotion.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Promotion extends Model
 {
-    //
+    protected $table = 'promotions';
+
+    public $primaryKey = 'id';
+    public $user_id;
+    public $offer_id;
+    public $code;
+    public $status = false;
+
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
