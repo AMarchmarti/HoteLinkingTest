872e1088be5afe9c9f713d7081211df4ae8d988c
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 03:47:22 +0100

    cambiado el index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Bienvenido,</div>
-                
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Has iniciado sesión correctamente.
-                </div>
-            </div>
-        </div>
-		
+    <div class="jumbotron text-center">
+        <h1>Welcome To Laravel!</h1>
+        <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p>
+        <p><a class="btn btn-primary btn-lg" href="http://localhost/HoteLinkingTest/public/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="http://localhost/HoteLinkingTest/public/register" role="button">Register</a></p>
     </div>
-</div>
 @endsection
