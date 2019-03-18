d9f43e8141697b646730effdb57a53e07c91ea69
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-03-18 11:44:40 +0100

    arreglado error, no salia por pantalla

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -3,16 +3,18 @@
 
 @section('content')
     <p>Aquí encontrará diferentes ofertas que le puedan interesar, ya sean noches gratuitas, descuentos para hoteles, entre otros...</p>
+    {!! Form::open(['action' => 'PromotionsController@store', 'method' => 'POST']) !!}
     @if(count ($offers) > 0)
         
-            {{!! Form::open(['action' => 'PromotionsController@store', 'method' => 'POST']) !!}}
+            
             @foreach($offers as $offer)
                 <div class="form-group">
                     {{ Form::label('title', $offer->Offers_name)}}
                     {{ Form::label('body', $offer->description)}}
                 </div>
                 {{ Form::submit('Canjear Cupón', ['class' => 'btn  btn-primary']) }}
-           {{ !! Form::close() }}
+          
         @endforeach
     @endif
+    { !!Form::close()!!}
 @endsection
